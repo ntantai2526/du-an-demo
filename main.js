4ec037d15aa62ee09a8674625c4e8c8151c1a51f
ntantai2526 <ntantai2526@gmail.com>
2025-09-28 14:50:07 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+//Hàm hiển thị hộp thoại lên trên trình duyệt
+// alert("Xin chao")
+// Hàm hiển thị hộp dữ liệu cho người dùng
+
+/*
+comment nhiều dòng
+*/
+// prompt("Mời bạn nhập vào tên bạn: ")
+//khai báo biến
+
+// // var myName;
+// let yourName;
+// // const doSoi;
+
+// //khỏi tạo biến
+
+// var diemToan = 10;
+// const doSoi = 100;
+// let firstName = "Tan Tai";
+
+// //Xuất giá trị của biến
+
+// console.log(diemToan)
+// console.log("Điểm toán của bạn là: " + diemToan + " điểm")
+// console.log(doSoi)
+// console.log(firstName)
+
+
+// //thử thay đổi giá trị của biến
+
+// diemToan = 5
+// console.log("Điểm toán của bạn là: " + diemToan + " điểm")
+// firstName = "Jayce"
+// console.log(firstName)
+
+// Bài tập lấy thông tin trong hàm prompt() và hiển thị
+
+let myName = prompt("Nhập tên của bạn: ");
+console.log("Xin chào "+ myName + ", bạn đã và đang học JS tại tuhoc.cc");
+
+
+
